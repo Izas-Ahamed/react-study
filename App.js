b8e75721ad1a8a4eb57a5adf3c99fdb668fc9c36
THUNDERX <thuderxcodes@gmail.com>
2024-01-17 12:39:31 +0530

    Revert "adding siblings"
    This reverts commit a0619bb3f35ba4f31ae4b8a3027bf3a952fcea69.

--- App.js
+++ App.js
@@ -3,7 +3,6 @@
 *<div id="parent">
     <div id="child">
         <h1> Hi from react </h1>
-        <h2> Hello from react </h1>
     </div>
 </div>
 *
@@ -13,13 +12,14 @@
 const element = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hi from React"),
-    React.createElement("h2", {}, "Hello from React"),
-  ])
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hi from React")
+  )
 );
 
-console.log(element); // prints object
+console.log(element);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

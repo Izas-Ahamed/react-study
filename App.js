047b0ea61b777001a25df33066f48b75dad39194
THUNDERX <thuderxcodes@gmail.com>
2024-01-17 12:44:55 +0530

    added more siblings

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 //nested html structure
 /*
 *<div id="parent">
-    <div id="child">
+    <div id="child1">
         <h1> Hi from react </h1>
+        <h2> Hello from react </h1>
+    </div>
+    <div id="child2">
+        <h1> Hi from react </h1>
+        <h2> Hello from react </h1>
     </div>
 </div>
 *
 *
 */
 
-const element = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hi from React")
-  )
-);
+//throws warning =>  Each child in a list should have a unique "key" prop
+const element = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi from React"),
+    React.createElement("h2", {}, "Hello from React"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi from React"),
+    React.createElement("h2", {}, "Hello from React"),
+  ]),
+]);
 
-console.log(element);
+console.log(element); // prints object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

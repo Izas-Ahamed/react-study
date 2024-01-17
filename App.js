17afb3810edaae6cc0077fb13b0a38d9b9f06076
THUNDERX <thuderxcodes@gmail.com>
2024-01-17 12:12:59 +0530

    object

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
-const h1 = React.createElement("h1", {}, "Hello World from React");
+const h1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+console.log(h1); // print object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);

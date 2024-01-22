e9794718c5a2dd29ec91d2688d739921d0b5a9d1
THUNDERX <thuderxcodes@gmail.com>
2024-01-22 12:02:42 +0530

    added routing

--- App.js
+++ App.js
@@ -2,16 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-
       <Body />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,17 @@
 import { IMG_URL } from "../utils/constants";
 
 const RestaurantCard = ({ resData }) => {
-  const { name, cuisines, avgRating, cloudinaryImageId } = resData?.info;
+  const { name, cuisines, avgRating, cloudinaryImageId, costForTwo, sla } =
+    resData?.info;
+  console.log(sla);
   return (
     <div className="res-card">
       <img alt="card-img" src={IMG_URL + cloudinaryImageId}></img>
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.slaString}</h4>
     </div>
   );
 };

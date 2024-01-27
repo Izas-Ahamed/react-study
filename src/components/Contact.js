e3d7ad17838dbe9a21125aa27d9dac6b845267c5
THUNDERX <thuderxcodes@gmail.com>
2024-01-27 15:24:27 +0530

    added test case for react component(throws error - no jsx babel parser)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,22 @@
 const Contact = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <div>
+      <h1 className="font-bold text-2xl p-4 m-4">Contact Us</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="UserName"
+          className="border border-black rounded-lg p-2 m-2"
+        />
+        <input
+          type="text"
+          placeholder="Message"
+          className="border border-black rounded-lg p-2 m-2"
+        />
+        <button className="rounded-lg bg-slate-400 ">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;

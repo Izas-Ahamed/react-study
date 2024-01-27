d5385d0e1961567c1373ef6775504eabcdee3dee
THUNDERX <thuderxcodes@gmail.com>
2024-01-27 16:30:00 +0530

    grouped test case by describe function

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,40 +2,43 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+//grouping test by describe
+describe("Contact us pages tests", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  //Assertion
-  expect(heading).toBeInTheDocument();
-});
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load button inside contact component", () => {
-  render(<Contact />);
+  test("Should load button inside contact component", () => {
+    render(<Contact />);
 
-  const button = screen.getByText("Submit");
+    const button = screen.getByText("Submit");
 
-  //Assertion
-  expect(button).toBeInTheDocument();
-});
+    //Assertion
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load input tag with Placeholder as UserName inside contact component", () => {
-  render(<Contact />);
+  test("Should load input tag with Placeholder as UserName inside contact component", () => {
+    render(<Contact />);
 
-  const userName = screen.getByPlaceholderText("UserName");
+    const userName = screen.getByPlaceholderText("UserName");
 
-  //Assertion
-  expect(userName).toBeInTheDocument();
-});
+    //Assertion
+    expect(userName).toBeInTheDocument();
+  });
 
-test("Should load 2 input tags inside contact component", () => {
-  render(<Contact />);
-  //input -textbox
-  const inputBoxes = screen.getAllByRole("textbox");
+  test("Should load 2 input tags inside contact component", () => {
+    render(<Contact />);
+    //input -textbox
+    const inputBoxes = screen.getAllByRole("textbox");
 
-  //console.log(inputBoxes);
+    //console.log(inputBoxes);
 
-  //Assertion
-  expect(inputBoxes.length).toBe(2);
+    //Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
 });

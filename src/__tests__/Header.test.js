1f2c75497fdc7d9c9a43e77760af11b81df951aa
THUNDERX <thuderxcodes@gmail.com>
2024-01-27 19:34:35 +0530

    added test for Cart link with Regex

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 import { Provider } from "react-redux";
 
-it("Should render header component", () => {
+it("Should render header component and contain login button", () => {
   render(
     //without this <Link/> error thrown
     <BrowserRouter>
@@ -19,4 +19,21 @@ it("Should render header component", () => {
   const loginButton = screen.getByRole("button", { name: "Login" });
 
   expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render header component and contain cart link", () => {
+  render(
+    //without this <Link/> error thrown
+    <BrowserRouter>
+      {/*without this redux error throwm*/}
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  //using regex
+  const cartText = screen.getByText(/Cart/);
+
+  expect(cartText).toBeInTheDocument();
 });
